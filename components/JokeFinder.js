a6fcd925d92c64ea81561d660fcde59868c8357e
codebyly <findichnett@gmx.de>
2021-04-10 15:37:43 +0200

    Joke lädt auf click

--- components/JokeFinder.js
+++ components/JokeFinder.js
@@ -1,46 +1,43 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 // "https://official-joke-api.appspot.com/random_joke"
 
 export default function JokeFinder() {
   const [joke, setJoke] = useState("");
   const [punch, setPunch] = useState("");
-  useFetchJoke(setJoke, setPunch);
+  const api = "https://official-joke-api.appspot.com/random_joke";
+
+  const loadJoke = () => {
+    fetchJoke(api, setJoke, setPunch);
+  };
 
   return (
-    <div className="quiz">
-      {joke && <p>{joke}</p>}
-      {punch && <p>{punch}</p>}
-    </div>
+    <>
+      <button onClick={loadJoke}>Show New Joke</button>
+
+      {joke && punch && (
+        <div className="quiz">
+          <p>{joke}</p>
+          <p>{punch}</p>
+        </div>
+      )}
+    </>
   );
 }
 
-function useFetchJoke(setJoke, setPunch) {
-  useEffect(() => {
-    async function fetchJoke() {
-      try {
-        const resp = await fetch(
-          `https://official-joke-api.appspot.com/random_joke`
-        );
+async function fetchJoke(api, setJoke, setPunch) {
+  try {
+    const resp = await fetch(api);
 
-        if (!resp.ok) {
-          throw new Error("Fehler beim Laden");
-        }
-
-        const jokeData = await resp.json();
-        // console.log(jokeData.setup);
-        // console.log(jokeData.punchline);
-        // const joke = `${jokeData.setup}...${jokeData.punchline}`;
-
-        // console.log(jokeData);
-        setJoke(jokeData.setup);
-        setPunch(jokeData.punchline);
-
-        // console.log(jokeData);
-      } catch (error) {
-        console.log(error);
-      }
+    if (!resp.ok) {
+      throw new Error("Fehler beim Laden");
     }
-    fetchJoke();
-  }, [setJoke, setPunch]); //END useEffect
+
+    const jokeData = await resp.json();
+
+    setJoke(jokeData.setup);
+    setPunch(jokeData.punchline);
+  } catch (error) {
+    console.log(error);
+  }
 }
