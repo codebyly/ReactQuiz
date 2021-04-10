2a53775e9aa6d94a5e7baf498b856dc17d1d4e72
codebyly <findichnett@gmx.de>
2021-04-10 16:27:26 +0200

    überflüssiges gelöscht

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Layout from "../components/Layout";
-import Link from "next/link";
 import QuizKonfigurator from "../components/QuizKonfigurator";
 
 export default function Home() {
@@ -7,23 +6,6 @@ export default function Home() {
     <Layout title="Quiz">
       {/* Version 1 */}
       <QuizKonfigurator />
-
-      {/* Version2 */}
-      {/* <QuizFetcher /> */}
     </Layout>
-
-    // <Layout title="Hallo!">
-    //   <div className="quiz">
-    //     <p>Hier gibt es manches zu entdecken</p>
-    //     <Link href="/quiz">
-    //       <button>Quiz</button>
-    //     </Link>
-
-    //     <Link href="/joke">
-    //       <button>Joke</button>
-    //     </Link>
-    //   </div>
-    // </Layout>
   );
-  //title als prob mitgeben
 }
